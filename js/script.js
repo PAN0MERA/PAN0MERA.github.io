1215484db685455945ef28bb7ad57318edecc262
Artem Ponomarev <artess.ponomarev@mail.ru>
2021-02-25 21:56:02 +0300

    My site

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var s = $(this).scrollTop();
+    var w = $(this).outerWidth();
+    var h = $('header').outerHeight();
+    var p = s/h *200;
+    var o = 0.5 - p/100;
+
+    var z = 1+(w/5000*p);
+
+    $('.parallax').css('transform', 'scale('+z+')');
+    $('.parallax').css('opacity', o);
+    if(o<0){
+      $('.parallax').css('display', 'none');
+    }
+    else{
+      $('.parallax').css('display', 'block');
+    }
+  });
+  $("#menu").on('click', function(){
+    $(this).toggleClass('btn-menu-active');
+    $('.nav-menu').slideToggle();
+    // $('.nav-menu').toggleClass('nav-menu-active');
+  });
+  $(".nav-menu, header").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+});
+});
